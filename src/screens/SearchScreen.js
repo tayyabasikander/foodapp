855cba4b180c0910aa69db90cc3ebef19c848f49
tayyabasikander2000 <tayyaba.sikander2000@gmail.com>
2020-02-07 21:12:35 +0500

    api edited

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,27 +1,47 @@
-import React, {useState} from 'react';
-import {View ,Text , StyleSheet} from 'react-native'; 
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp' ;
+import yelp from '../api/yelp';
+
 
 const SearchScreen = () => {
-    const [term,setTerm] = useState('')
-    const [results,setResults] = useState([])
+    const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+    const [errorMessage,setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        console.log(searchTerm);
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+        }
+        catch (err) {
+            setErrorMessage('something went wrong');
+        }
+    }
+    useEffect( () => {
+        searchApi ('pasta');
+    }, [] )
+
     return (
         <View>
-
             <SearchBar
-            term = {term} 
-            onTermChange={newValue => { 
-                setTerm(newValue) 
-            }}
-            onTermSubmit={()=>console.log('term was submitted')}
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(terms)}
             />
-            <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            {errorMessage ?  <Text> {errorMessage} </Text> : null }   
+            <Text>We have found {results.length} results</Text>
         </View>
     )
 }
-const styles= StyleSheet.create({
-      
+const styles = StyleSheet.create({
+
 })
 export default SearchScreen;
