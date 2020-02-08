d053698c5e22e41ac25c18f1b48c5b8eae6c0684
tayyabasikander2000 <tayyaba.sikander2000@gmail.com>
2020-02-08 19:33:11 +0500

    Food app completed

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,47 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet,ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
-
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
+    //console.log("props are", props);
     const [term, setTerm] = useState('')
-    const [results, setResults] = useState([])
-    const [errorMessage,setErrorMessage] = useState('');
+    const [ searchApi , results , errorMessage ] = useResults();
+    //console.log(results);
 
-    const searchApi = async (searchTerm) => {
-        console.log(searchTerm);
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            });
-            setResults(response.data.businesses);
-        }
-        catch (err) {
-            setErrorMessage('something went wrong');
-        }
-    }
-    useEffect( () => {
-        searchApi ('pasta');
-    }, [] )
+    const filterResultsByPrice = (price) => {
+        //price ==='$' || '$$' || '$$$'
+        return results.filter(i=>{
+            return i.price === price;
+        });
+    };
 
     return (
-        <View>
+        < >
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={() => searchApi(terms)}
+                onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ?  <Text> {errorMessage} </Text> : null }   
-            <Text>We have found {results.length} results</Text>
-        </View>
+            {/* <Text style={{marginLeft:15}}>We have found {results.length} results</Text> */}
+            <ScrollView>
+            <ResultsList results={filterResultsByPrice('$')} title="Cost effective"/>
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
+            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender"/>
+            </ScrollView>
+        </>
     )
 }
 const styles = StyleSheet.create({
+
 
 })
 export default SearchScreen;
